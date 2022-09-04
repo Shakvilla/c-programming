d0562bb86552f4960f950a182d9fa6a7bf544f49
Shakvilla <abdulshakuraclement@yahoo.com>
2022-09-04 00:08:26 +0000

    added exit success

--- hello.c
+++ hello.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void)
-{
-  printf("Hello, world. I'm learning C Programming again!\n");
-  return 0;
+
+int main(void){
+  printf("Hello World!\n");
+  return EXIT_SUCCESS;
 }
